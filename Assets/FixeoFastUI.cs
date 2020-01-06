4c0d9c82b8cd734a1153437f077653a9ec071d75
André <andrescrich@hotmail.com>
2020-01-06 15:29:49 +0100

    Cambios que se hicieron en la exposicion

---
+++ Assets/FixeoFastUI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixeoFastUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 position;
+    private void Awake()
+    {
+        position = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = position;
+    }
+}
